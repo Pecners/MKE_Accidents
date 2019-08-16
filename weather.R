28f4c951bfc9718945ac556ed99b0f6093a32f9b
Spencer <spencer.schien@gmail.com>
2019-08-16 16:02:40 -0500

    Updating weather data import--NOT FINISHED

--- weather.R
+++ weather.R
@@ -1,7 +1,17 @@
 library(tidyverse)
 library(lubridate)
 
-weather <- read_csv("mke_weather.csv")
+data1 <- read_csv("data/1840081.csv")
+data2 <- read_csv("data/1840086.csv")
+
+full_weather <- bind_rows(data1, data2)
+
+rm(data1)
+rm(data2)
+
+weather <- full_weather %>%
+  select(date = DATE,
+         )
 
 daily_w <- weather %>%
   group_by(date = mdy(DATE)) %>%
@@ -9,3 +19,4 @@ daily_w <- weather %>%
             snow = mean(SNOW)) %>%
   arrange(date)
 
+x <- c("Spencer", "Schien")
